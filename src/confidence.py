d80fcbef835c9be74cf2d7af2013832ddcea5073
Ngha_Boris <borisngha6@gmail.com>
2023-12-09 12:56:15 +0100

    Confidence: Have implemented the confidence funnction.

--- src/confidence.py
+++ src/confidence.py
@@ -1,9 +1,33 @@
 dataset_type = list[list[object]]
 rule_type = tuple[list[object], list[object]]
 
-
+from typing import List, Tuple
 def confidence(data_set: dataset_type, rule: rule_type) -> float:
     """
     To measure the likelihood of occurrence of an itemset given another itemset.
     """
-    pass
+
+    if premise_support == 0:
+        return 0.0
+
+    confidence_value = rule_support / premise_support
+    return confidence_value
+
+# Example Usage:
+
+    # Example dataset
+dataset = [
+        ['apple', 'banana', 'cherry'],
+        ['banana', 'orange'],
+        ['apple', 'banana', 'cherry', 'orange'],
+        ['apple', 'cherry'],
+        ['banana', 'cherry'],
+    ]
+
+    # Example rule
+rule = (['apple'], ['banana'])
+
+    # Calculate confidence
+confidence_value = confidence(dataset, rule)
+
+print(f"The confidence of the rule {rule} in the dataset is: {confidence_value}")
