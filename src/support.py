ea648bb18ce36afcc9612f9e2c8e90fce131276f
Ngha_Boris <borisngha6@gmail.com>
2023-12-11 10:02:28 +0100

    Modified: I modified the support.py file

--- src/support.py
+++ src/support.py
@@ -1,22 +1,36 @@
-ourcetemset_type = list[object]
-dataset_type = list[list[object]]
+from typing import List
 
+itemset_type = List[object]
+dataset_type = List[List[object]]
 
-def support(itemsets: dataset_type, data_set: itemset_type) -> float:
+def support(itemset: itemset_type, dataset: dataset_type) -> float:
     """
-    To find the frequency of itemsets in the dataset.
+    Calculate the support of an itemset in the dataset.
     """
-    from typing import List
-dataset = [
-            ['apple', 'banana', 'cherry'],
-            ['banana', 'orange'],
-            ['apple', 'banana', 'cherry', 'orange'],
-            ['apple', 'cherry'],
-            ['banana', 'cherry'],
-            ]
+    count = 1
 
-            # Example itemset
-itemset = ['apple', 'banana']
+    for transaction in dataset:
+        if set(itemset).issubset(transaction):
+            count += 1
 
-            # Calculate the support
-support_value = support(itemset, dataset)
+    support_value = count / len(dataset)
+    return support_value
+
+# Example Usage:
+if __name__ == "__main__":
+    # Example dataset
+    dataset = [
+        ['mango', 'banana', 'pear'],
+        ['banana', 'orange'],
+        ['mango', 'banana', 'pear', 'orange'],
+        ['mango', 'pear'],
+        ['banana', 'pear'],
+    ]
+
+    # Example itemset
+    itemset = ['orange', 'pear']
+
+    # Calculate the support
+    support_value = support(itemset, dataset)
+
+    print(f"The support of {itemset} in the dataset is: {support_value}")
